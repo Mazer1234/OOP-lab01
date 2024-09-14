d9618a5790ad1c2c07092d5a6902d7487e667c31
Leonid <Leopas05@yandex.ru>
2024-09-14 10:31:24 +0300

    Добавил Отчёты

--- src/Main.java
+++ src/Main.java
@@ -32,14 +32,14 @@ public class Main {
     public static void sort(int[] array, boolean ascending){
         int n = array.length;
         boolean swap;
-        for (int i = 0; i < n; i++){
+        for (int i = 0; i < n - 1; i++){
             swap = false;
-            for (int j = i; j < n-1; j++){
+            for (int j = 0; j < n-1 - i; j++){
                 if ((array[j] > array[j+1] && ascending) || (array[j] < array[j+1] && !ascending)) {
-                    int temp;
-                    temp = array[j];
+                    int temp = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = temp;
+                    swap = true;
                 }
             }
             if (!swap){
